d5cd510cf7a4cd30226da6ce8a90988043ecb55d
Sumen del Rosario <sumen.delrosario@gmail.com>
2023-03-13 08:22:05 +0800

    admin navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,41 @@
-import { AppBar, Button } from '@mui/material'
+import Link from '@/components/Link'
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import { Container } from '@mui/system'
 import { signOut } from 'next-auth/react'
 
 export const NavBar = () => {
   return (
     <AppBar>
-      <Button color='inherit' onClick={() => signOut()}>
-        Logout
-      </Button>
+      <Container maxWidth='xl'>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, width: '100%', display: 'flex' }}>
+            <Typography
+              variant='body1'
+              sx={{ color: '#FFF', mr: 2, fontWeight: 900 }}
+            >
+              Beyond Ripped
+            </Typography>
+            <Typography sx={{ color: '#FFF' }}>|</Typography>
+            <Link
+              sx={{ textDecoration: 'none', mr: 2, ml: 2 }}
+              color='#FFF'
+              href='/admin/members'
+            >
+              Members
+            </Link>
+            <Link
+              sx={{ textDecoration: 'none', mr: 2 }}
+              color='#FFF'
+              href='/admin/finance'
+            >
+              Finance
+            </Link>
+          </Box>
+          <Button color='inherit' onClick={() => signOut()}>
+            Logout
+          </Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
