c86eeddc0312660625eff0c1dd9411b0840c8f52
Sumen del Rosario <sumen.delrosario@gmail.com>
2023-03-30 16:29:28 +0800

    try send error via ses

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,6 +1,7 @@
 import * as aws from '@aws-sdk/client-ses'
 import { defaultProvider } from '@aws-sdk/credential-provider-node'
 import nodemailer from 'nodemailer'
+import smtpTransport from 'nodemailer-smtp-transport'
 
 const ses = new aws.SES({
   region: 'ap-northeast-1',
@@ -8,7 +9,7 @@ const ses = new aws.SES({
   defaultProvider
 })
 
-export const sendMail = async (message: any) => {
+export const sendLogMail = async (message: any) => {
   try {
     const transporter = nodemailer.createTransport({
       SES: { ses, aws }
@@ -21,7 +22,7 @@ export const sendMail = async (message: any) => {
             from: 'admin@beyondripped.ph',
             to: 'admin@beyondripped.ph',
             subject: 'Message',
-            text: 'This is received',
+            text: message,
             ses: {
               // optional extra arguments for SendRawEmail
               Tags: [
@@ -41,4 +42,47 @@ export const sendMail = async (message: any) => {
   } catch (error) {
     console.log(error)
   }
+}
+
+export const sendMail = async (message: any) => {
+  /*
+    service: 'GoDaddy',
+    host: "smtp.office365.com",  
+    secure: false,
+    port: 587,
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+    } 
+  */
+
+  /*
+    service: 'gmail',
+      host: 'smtp.gmail.com',
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    */
+  const mailer = nodemailer.createTransport(
+    smtpTransport({
+      host: 'beyondripped.ph',
+      port: 465,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      },
+      tls: { rejectUnauthorized: false }
+    })
+  )
+
+  await mailer.sendMail(message, (error: any, info: any) => {
+    if (error) {
+      const message = typeof error === 'object' ? JSON.stringify(error) : error
+      sendLogMail(message)
+      return console.log(error)
+    }
+    sendLogMail(info?.response)
+    console.log('Message sent: %s', info)
+  })
 }
