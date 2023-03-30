9e784c9c5325e7b9ba40a81660004249966298a1
Sumen del Rosario <sumen.delrosario@gmail.com>
2023-03-30 17:38:08 +0800

    remove aws sdk

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,48 +1,5 @@
-import * as aws from '@aws-sdk/client-ses'
-import { defaultProvider } from '@aws-sdk/credential-provider-node'
 import nodemailer from 'nodemailer'
 import smtpTransport from 'nodemailer-smtp-transport'
-
-const ses = new aws.SES({
-  region: 'ap-northeast-1',
-  //@ts-ignore
-  defaultProvider
-})
-
-export const sendLogMail = async (message: any) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      SES: { ses, aws }
-    })
-
-    transporter.once('idle', () => {
-      if (transporter.isIdle()) {
-        transporter.sendMail(
-          {
-            from: 'admin@beyondripped.ph',
-            to: 'admin@beyondripped.ph',
-            subject: 'Message',
-            text: 'There is an error',
-            ses: {
-              // optional extra arguments for SendRawEmail
-              Tags: [
-                {
-                  Name: 'tag_name',
-                  Value: 'tag_value'
-                }
-              ]
-            }
-          },
-          (err: any, info: any) => {
-            console.log({ err, info })
-          }
-        )
-      }
-    })
-  } catch (error) {
-    console.log(error)
-  }
-}
 
 export const sendMail = async (message: any) => {
   return new Promise((resolve, reject) => {
