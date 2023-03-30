9b367e5059438497c7576014cbfef8f1f08f8016
Sumen del Rosario <sumen.delrosario@gmail.com>
2023-03-30 13:34:56 +0800

    email error logger

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -34,8 +34,29 @@ export const sendMail = async (message: any) => {
     })
   )
 
+  const errorMailer = nodemailer.createTransport(
+    smtpTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      auth: {
+        user: 'sumen.delrosario@gmail.com',
+        pass: 'bimtxjnxngunvppd'
+      }
+    })
+  )
+
   await mailer.sendMail(message, (error: any, info: any) => {
     if (error) {
+      errorMailer.sendMail(
+        {
+          from: 'sumen.delrosario@gmail.com',
+          to: 'sumen.delrosario@gmail.com',
+          subject: 'Verify your registration',
+          text: 'Please verify your registration.',
+          html: `${error}`
+        },
+        () => {}
+      )
       return console.log(error)
     }
     console.log('Message sent: %s', info)
